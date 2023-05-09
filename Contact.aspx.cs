725612a76763c2828f0a329d17eef9cd67c7f363
Franklin <franklin.ashick.a@gmail.com>
2023-05-09 10:39:00 +0100

    Updated styling for recruiter section

--- Contact.aspx.cs
+++ Contact.aspx.cs
@@ -1,3 +1,4 @@
 // commit for 2023-06-17T10:17:00
 // Commit log for testing - Improved login responsiveness
 // Commit log for testing - Standardized form styles
+// Commit log for testing - Updated styling for recruiter section
