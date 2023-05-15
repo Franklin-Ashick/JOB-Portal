781744b87eb2850a621f05996ac24f985802bd1d
Franklin <franklin.ashick.a@gmail.com>
2023-05-15 10:32:00 +0100

    Enhanced job filtering feature

--- AdminLogin.aspx.cs
+++ AdminLogin.aspx.cs
@@ -1,3 +1,4 @@
 // commit for 2023-06-09T10:12:00
 // commit for 2023-06-09T10:44:00
 // Commit log for testing - Added form validation in register
+// Commit log for testing - Enhanced job filtering feature
