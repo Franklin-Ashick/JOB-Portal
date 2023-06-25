179869882b50425792479ca7c10173fb0152200c
Franklin <franklin.ashick.a@gmail.com>
2023-06-25 10:50:00 +0100

    Created helper class for login

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -11,3 +11,4 @@
 // Commit log for testing - Created helper class for login
 // Commit log for testing - Cleaned up CSS classes
 // Commit log for testing - Updated styling for recruiter section
+// Commit log for testing - Created helper class for login
