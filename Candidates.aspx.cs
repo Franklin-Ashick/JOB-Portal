c571b9e468fa13c03b6181bffc17a07941a13a4e
Franklin <franklin.ashick.a@gmail.com>
2023-06-25 10:44:00 +0100

    Implemented email verification

--- Candidates.aspx.cs
+++ Candidates.aspx.cs
@@ -3,3 +3,4 @@
 // commit for 2023-05-10T10:48:00
 // commit for 2023-06-07T10:42:00
 // Commit log for testing - Standardized form styles
+// Commit log for testing - Implemented email verification
